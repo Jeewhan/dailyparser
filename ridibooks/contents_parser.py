1ef276b977899a39a5d18872bdea7ce91e6ec0c7
Beomi <jun@beomi.net>
2016-12-23 19:43:03 +0900

    Let selenium use PhantomJS  / Add Implicit waits

--- ridibooks/contents_parser.py
+++ ridibooks/contents_parser.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from time import sleep
+import json
 
 
 def get_content(user_id, user_pw, booknumber):
@@ -8,34 +9,44 @@ def get_content(user_id, user_pw, booknumber):
     def get_htmls(url, max_try_times):
         try_time = 1
         def get_html_raw(url, try_time):
-            if try_time <= max_try_times:
-                try:
-                    driver.get(url)
-                    sleep(try_time)
-                    driver.find_element_by_id('ridi_c1')  # check if texts are loaded or not
-                    htmls = driver.find_elements_by_tag_name('article')
-                    return htmls
-                except:
-                    htmls = get_html_raw(url=url,try_time=try_time+1)
-                    return htmls
-            else:
-                input('Plz login again (or) Buy books')
+            try:
+                driver.get(url)
+                driver.implicitly_wait(15)
+                driver.find_element_by_id('ridi_c1')  # check if texts are loaded or not
+                htmls = driver.find_elements_by_tag_name('article')
+                return htmls
+            except:
+                sleep(2)
+                driver.get(base_url + "account/login")
+                driver.find_element_by_name("user_id").clear()
+                driver.find_element_by_name("user_id").send_keys(user_id) #str type, input RIDI ID
+                driver.find_element_by_name("passwd").clear()
+                driver.find_element_by_name("passwd").send_keys(user_pw) #str type, input RIDI PW
+                driver.find_element_by_css_selector("button.blue_button.submit_button").click()
+
+                sleep(2)
+                htmls = get_html_raw(url, try_time)
+                return htmls
+
         htmls = get_html_raw(url, try_time)
         return htmls
 
+    
+        
 
-    #driver open with Firefox
-    driver = webdriver.Firefox()
-    base_url = "http://ridibooks.com/"
-
-    #login
+    driver = webdriver.PhantomJS('phantomjs')
+    driver.implicitly_wait(3)
+    base_url = "https://ridibooks.com/"
+    #driver = webdriver.Chrome('chromedriver')
     driver.get(base_url + "account/login")
     driver.find_element_by_name("user_id").clear()
     driver.find_element_by_name("user_id").send_keys(user_id) #str type, input RIDI ID
     driver.find_element_by_name("passwd").clear()
     driver.find_element_by_name("passwd").send_keys(user_pw) #str type, input RIDI PW
-    driver.find_element_by_id('remember_login').click()
     driver.find_element_by_css_selector("button.blue_button.submit_button").click()
+
+    sleep(2)
+
 
     #get lists
     book_list = open('book_list_{}.txt'.format(booknumber),'r').readlines()
@@ -45,11 +56,14 @@ def get_content(user_id, user_pw, booknumber):
     #input max_retry_times
     max_try_times = 3
 
-    for book in book_list:
+    book_length = len(book_list)
+
+    for idx, book in enumerate(book_list):
         url = str(book).replace('\n','')
         htmls = get_htmls(url, max_try_times)
         for html in htmls:
             f.write(html.text + '\n{}\n'.format('-' * 10))
+        print('({}%) {}/{}'.format(str(((idx+1)/book_length)*100)[:4], idx+1, book_length))
 
 
     f.close()
@@ -58,7 +72,10 @@ def get_content(user_id, user_pw, booknumber):
 
 
 if __name__ == "__main__":
-    user_id = input('Input your RIDI ID: ')
-    user_pw = input('Input your RIDI PW: ')
-    booknumber = input('Input booknumber: ')
+    env = open('envs.json')
+    env_json = json.load(env)
+    print(env_json)
+    user_id = env_json['ridi_id']
+    user_pw = env_json['ridi_pw']
+    booknumber = env_json['book_num']
     get_content(user_id, user_pw, booknumber)
